b53f77c876f21c01e1a335ed687e459a73449560
pryceholmes <pcholmes@asu.edu>
2024-01-21 10:31:55 -0600

    Initial Commit v0.0

---
+++ src/core/CheckersLogic.java
@@ -0,0 +1,11 @@
+package core;
+
+/**
+ * Game logic for Checkers Game
+ *
+ * @author Pryce Holmes
+ * @version v0.0
+ */
+public class CheckersLogic {
+
+}
---
+++ src/ui/CheckersTextConsole.java
@@ -0,0 +1,13 @@
+package ui;
+
+/**
+ * Text-based UI implementation of Checkers game
+ *
+ * @author Pryce Holmes
+ * @version 0.0
+ */
+public class CheckersTextConsole {
+    public static void main(String[] args) {
+
+    }
+}
